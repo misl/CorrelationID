c930a51a892fde4afdcdb5cc7930198fac2df700
Minto van der Sluis <minto@xup.nl>
2017-10-20 12:17:10 +0200

    Ensure read/create correlation ID as soon as possible, during authentication

--- correlation-jaxrs/src/main/java/nl/xup/web/correlation/jaxrs/server/CorrelationIDContainerResponseFilter.java
+++ correlation-jaxrs/src/main/java/nl/xup/web/correlation/jaxrs/server/CorrelationIDContainerResponseFilter.java
@@ -2,6 +2,8 @@ package nl.xup.web.correlation.jaxrs.server;
 
 import java.io.IOException;
 
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
@@ -17,6 +19,7 @@ import nl.xup.web.correlation.core.CorrelationIDManager;
  * @author misl
  */
 @Provider
+@Priority(Priorities.AUTHENTICATION)
 public class CorrelationIDContainerResponseFilter implements ContainerResponseFilter {
 
   // --------------------------------------------------------------------------
